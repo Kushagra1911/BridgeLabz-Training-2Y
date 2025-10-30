268e1e5d7e505ece5cf5905ef14206deed25ea66
Kushagra1911 <yadavkushagra97@gmail.com>
2025-10-31 00:53:33 +0530

    Delete HarryAge.java

--- HarryAge.java
+++
@@ -1,8 +0,0 @@
-class HarryAge {
-    public static void main(String[] args) {
-        int harrybirthyear = 2000;
-        int currentYear = 2024;
-        int harryAge = currentYear - harrybirthyear;
-        System.out.println("Harry's age is: " + harryAge);
-    }
-}
